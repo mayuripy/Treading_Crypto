db98af933d9b4b2ee496b655ee6bc731e61d7583
mayuripy <chaudharimayuri2004@gmail.com>
2025-08-21 23:26:59 +0530

    confige some

---
+++ Treading_Crypto/src/main/java/com/sit/Config/AppConfig.java
@@ -0,0 +1,29 @@
+package com.sit.Config;
+
+
+import jakarta.servlet.Filter;
+import org.apache.tomcat.util.file.ConfigurationSource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AppConfig {
+  
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		http.sessionManagement(managment ->managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).
+		authorizeHttpRequests(Authorizae->Authorizae.requestMatchers("/api/**").authenticated().anyRequest().permitAll())
+				.addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+				.csrf(csrf->csrf.disable())
+				.cors(cors->cors.ConfigurationSource(corsConfigrationSource()));
+		
+		return null;
+	}
+	
+	private ConfigurationSource corsConfigrationSource() {
+		
+		return null;
+	}
+	 
+}
