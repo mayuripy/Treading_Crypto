e6cde7c4dedcc145ed071225f2efd3835ccc2320
mayuripy <chaudharimayuri2004@gmail.com>
2025-08-25 23:27:35 +0530

    Appconfige

--- Treading_Crypto/src/main/java/com/sit/Config/AppConfig.java
+++ Treading_Crypto/src/main/java/com/sit/Config/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class AppConfig {
@@ -14,7 +15,7 @@ public class AppConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		http.sessionManagement(managment ->managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).
 		authorizeHttpRequests(Authorizae->Authorizae.requestMatchers("/api/**").authenticated().anyRequest().permitAll())
-				.addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+				.addFilterBefore(new JWtTokenValidatore(), BasicAuthenticationFilter.class)
 				.csrf(csrf->csrf.disable())
 				.cors(cors->cors.ConfigurationSource(corsConfigrationSource()));
 		
