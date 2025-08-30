0ed39972cde07a8d02f842934448c4fc80ecec8d
mayuripy <chaudharimayuri2004@gmail.com>
2025-08-30 23:26:14 +0530

    fixed bugs

--- Treading_Crypto/src/main/java/com/sit/Config/AppConfig.java
+++ Treading_Crypto/src/main/java/com/sit/Config/AppConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import ch.qos.logback.core.filter.Filter;
+
 @Configuration
 public class AppConfig {
   
