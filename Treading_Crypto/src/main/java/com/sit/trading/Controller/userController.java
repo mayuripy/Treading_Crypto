5c1044873e2051f9cde3e4f0aab154b04a1d945a
mayuripy <chaudharimayuri2004@gmail.com>
2025-09-16 00:00:07 +0530

    usercontroller

--- Treading_Crypto/src/main/java/com/sit/trading/Controller/userController.java
+++ Treading_Crypto/src/main/java/com/sit/trading/Controller/userController.java
@@ -1,5 +1,32 @@
 package com.sit.trading.Controller;
 
-public class userController {
 
+@RestController
+@RequestMapping("/api/users")
+@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+public class UserController {
+
+ @Autowired
+ private UserService userService;
+
+ @GetMapping("/{id}")
+ public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+     return ResponseEntity.ok(userService.getUserById(id));
+ }
+
+ @GetMapping
+ public ResponseEntity<List<UserDto>> getAllUsers() {
+     return ResponseEntity.ok(userService.getAllUsers());
+ }
+
+ @PostMapping
+ public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserCreateDto dto) {
+     return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(dto));
+ }
+
+ @DeleteMapping("/{id}")
+ public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id) {
+     userService.deleteUser(id);
+     return ResponseEntity.ok(Map.of("ok", true));
+ }
 }
