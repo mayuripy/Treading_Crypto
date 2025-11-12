36493a4dd26b5b3e955df113d5e3d53f52cb23bf
mayuripy <chaudharimayuri2004@gmail.com>
2025-11-12 19:48:26 +0530

    finally done

--- Treading_Crypto/src/main/java/com/sit/trading/Services/CustomUserDetailsService.java
+++ Treading_Crypto/src/main/java/com/sit/trading/Services/CustomUserDetailsService.java
@@ -42,6 +42,7 @@ public class CustomUserDetailsService implements UserDetailsService {
                 user.getEmail(),
                 user.getPassword(),
                 new ArrayList<>()
+                
         );
     }
 }
--- Treading_Crypto/src/main/java/com/sit/trading/model/User.java
+++ Treading_Crypto/src/main/java/com/sit/trading/model/User.java
@@ -14,7 +14,7 @@ import lombok.Data;
 
 @Entity
 @Data
-public class User {
+public class User{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
